cf0066ff94ce0c2aae3639cd2143ab0a57a750c6
Jepmoltho <jept@itu.dk>
2023-04-30 15:15:22 +0200

    Finished setting up the project and fixed TS dependency issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { reactLogo } from "./logos";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {reactLogo()}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
